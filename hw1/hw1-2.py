0a13d036f796d9cc6946a2c8e0cba8d564f3f81d
lyjeff <38328932+lyjeff@users.noreply.github.com>
2021-03-27 00:56:15 +0800

    Finish the codes
    - finish the code of PLA and Pocket alg.
    - TODO: write the report!!!

---
+++ hw1/hw1-2.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+from utils import (
+    rand_seed, PLA, get_sign,
+    build_data, verification, plt_proc
+)
+
+def PLA_3_times_with_30_data(m, b, num, times):
+    # build 2D data
+    x, y = build_data(m, b, num)
+
+    #initial weight w = (0, 0, 0)
+    w = np.zeros([1,3])
+
+    # count the iteration total numbers
+    iteration_count = 0
+
+    # initial the figure
+    plt_fig = plt_proc(x, num, title="HW1-2: PLA with 30 2D data samples")
+
+    # plot the sample line equation
+    plt_fig.add_line(
+        w=None,
+        m=m,
+        b=b,
+        num=num,
+        iteration=None,
+        label=f"Benchmark",
+        txt=""
+    )
+
+    for i in range(times):
+
+        # run PLA algorithm
+        w_result, iteration = PLA(x, y, w, num)
+
+        # verify the line equation
+        verification(x, y, w_result, num, iteration, show=True)
+
+        # count the total number of iterations
+        iteration_count += iteration
+
+        # plot the line equation
+        plt_fig.add_line(
+            w=w_result,
+            m=None,
+            b=None,
+            num=num,
+            iteration=iteration,
+            label=f"{i}",
+            txt=f", iteration = {iteration}"
+        )
+
+    print(f"Avg. Iteration = {iteration_count/3:.3f}")
+
+    # save and show the figure
+    plt_fig.save_and_show(
+        itr_avg=iteration_count / 3,
+        filename='hw1-2.png',
+        avg_show=True
+    )
+
+if __name__ == '__main__':
+    
+    # y = m * x + b
+    m = 1
+    b = 2
+    
+    # default number of data = 30
+    num = 30
+    
+    # set the running times of PLA
+    times = 3
+
+    PLA_3_times_with_30_data(m, b, num, times)
---
+++ hw1/utils.py
@@ -0,0 +1,144 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def rand_seed(m, b, num=2):
+    # create empty list
+    x_coor = []
+    y_coor = []
+    label = []
+    # positive and negtive point number
+    pos_num = int(num / 2)
+    neg_num = num - pos_num
+    # random create point
+    for i in range(pos_num):
+        x = random.uniform(0, 30)
+        r = random.uniform(1, 30)
+        y = m * x + b - r
+        # save the coordinate of x and y
+        x_coor.append(x)
+        y_coor.append(y)
+        # save label, right=1, left=0
+        label.append(1 if m >= 0 else -1)
+
+    for i in range(neg_num):
+        x = random.uniform(0, 30)
+        r = random.uniform(1, 30)
+        y = m * x + b + r
+        x_coor.append(x)
+        y_coor.append(y)
+        label.append(-1 if m >= 0 else 1)
+    return x_coor, y_coor, label
+
+def get_sign(w, x):
+    return np.sign(np.sum(w * x))
+
+def PLA(x, y, w, num):
+    iteration = 0
+    while True:
+        error_exist = False
+        rand_list = random.sample(range(num), num)
+        for i in rand_list:
+            if get_sign(w, x[i, :].reshape([1, 3])) != y[i]:
+                w = w + y[i] * x[i, :].reshape([1, 3])
+                error_exist = True
+                iteration += 1
+                break
+
+        if error_exist == False:
+            break;
+
+    return w, iteration
+
+def Pocket(x, y, w, num, threshold, continuous_threshold):
+    iteration = 0
+    count = 0
+    while iteration < threshold and count < continuous_threshold:
+        i = random.randint(0, num - 1)
+
+        if get_sign(w, x[i, :].reshape([1, 3])) != y[i]:
+            wt = w + y[i] * x[i, :].reshape([1, 3])
+            
+            error_rate = verification(x, y, w, num, iteration=None, show=False)
+            error_rate_t = verification(x, y, wt, num, iteration=None, show=False)
+
+            if error_rate_t < error_rate:
+                w = wt
+                count = 0
+            else:
+                count += 1
+
+            iteration += 1
+
+            if error_rate_t == 0:
+                break
+
+    return w, iteration
+
+def build_data(m, b, num):
+    # x = 2D data, (1, x0, x1) = (1, x, y)
+    x = np.zeros([num, 3])
+    
+    # label of data
+    y = np.zeros([num])
+
+    # build 2D data samples
+    x[:,1], x[:,2], y = rand_seed(m, b, num)
+    x[:, 0] = 1
+
+    return x, y
+
+def verification(x, y, w, num, iteration, show=False):
+    error_count = 0
+
+    for i in range(num):
+        if get_sign(w, x[i, :].reshape([1, 3])) != y[i]:
+            error_count += 1
+
+    if show == True:
+        print(f"y = {-w[0, 1] / w[0, 2]:.5f} * x + {-w[0, 0] / w[0, 2]:.5f}")
+        print(f"iteration = {iteration}")
+        if error_count == 0:
+            print("Correct Line Equation!!!")
+        else:
+            print("Wrong Line Equation!!!")
+
+    return error_count/num
+
+class plt_proc():
+
+    def __init__(self, x, num, title):
+        # set the figure
+        plt.figure(figsize=(13, 11))
+        plt.title(title)
+        plt.xlabel("x-axis")
+        plt.ylabel("y-axis")
+
+        # plot the data
+        plt.plot(x[:int(num/2), 1], x[:int(num/2), 2], 'o', label="data with label = 1")
+        plt.plot(x[int(num/2):, 1], x[int(num/2):, 2], 'x', label="data with label = -1")
+
+    def add_line(self, w, m, b, num, iteration, label, txt):
+        # plot the line equation
+        # get m and b from w
+        if w is not None:
+            m = -1 * w[0, 1] / w[0, 2]
+            b = -1 * w[0, 0] / w[0, 2]
+        
+        x = np.arange(30 + 1)
+        y = m * x + b
+
+        plt.plot(x, y, label=f"{label}: y = {m:.5f} * x + {b:.5f}{txt}")
+
+    def save_and_show(self, itr_avg, filename, avg_show=False):
+        if avg_show is True:
+            plt.title(f"Avg. Iteration = {itr_avg:.3f}", loc="right")
+
+        # set the legend of the figure
+        plt.legend(loc="best")
+
+        # save the figure
+        plt.savefig(filename, dpi=800, bbox_inches="tight")
+
+        # show the figure
+        plt.show()
